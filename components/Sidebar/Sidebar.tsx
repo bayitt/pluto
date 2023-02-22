85ddcde9dfe3ddcd114a3b1800b8b2f700c388d7
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-02-22 20:15:43 +0100

    feat: working on mobile sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { Box, VStack, Link, Button } from "@chakra-ui/react";
 import { TAppState } from "../../store";
 import { TSidebar } from "./types";
 
-export const Sidebar: FC<TSidebar> = () => {
+export const Sidebar: FC<TSidebar> = ({ open, handleOpen }) => {
   const { categories } = useSelector<TAppState, TAppState>((state) => state);
   const router = useRouter();
 
@@ -26,21 +26,36 @@ export const Sidebar: FC<TSidebar> = () => {
     ));
 
   return (
-    <Box
-      width={{ base: "80vw", sm: "50vw" }}
-      px={{ base: 8, md: 16 }}
-      pt={{ base: "35%", sm: "20%" }}
-      position="fixed"
-      top={0}
-      right={0}
-      height="100vh"
-      bg="white"
-      zIndex={10}
-    >
-      <VStack spacing={5} alignItems="flex-start">
-        {renderCategories()}
-        <Button>Subscribe</Button>
-      </VStack>
+    <Box>
+      <Box
+        right={{ base: open ? 0 : "-75vw", sm: open ? 0 : "-50vw" }}
+        width={{ base: "75vw", sm: "50vw" }}
+        px={{ base: 8, md: 16 }}
+        pt={{ base: "35%", sm: "20%" }}
+        position="fixed"
+        top={0}
+        height="100vh"
+        bg="white"
+        opacity={Number(open)}
+        zIndex={open ? 10 : -9999}
+      >
+        <VStack spacing={5} alignItems="flex-start">
+          {renderCategories()}
+          <Button>Subscribe</Button>
+        </VStack>
+      </Box>
+      <Box
+        position="fixed"
+        top={0}
+        left={0}
+        width="100vw"
+        height="100vh"
+        background="rgba(0,0,0,.2)"
+        zIndex={5}
+        onClick={() => {
+          handleOpen(false);
+        }}
+      />
     </Box>
   );
 };
